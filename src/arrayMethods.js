6890b0f58eb6c1798bc28475b3ca02b84e5e755b
Fernando <fernando82628@yahoo.com>
2022-12-21 18:12:10 -0600

    initialize repo

---
+++ src/arrayMethods.js
@@ -0,0 +1,58 @@
+// Array.map() review
+
+// const names = ['anne', 'henry', 'chloe'];
+// const bigNames = names.map((name) => name.toUpperCase());
+
+// console.log(bigNames);
+// console.log(names);
+
+//Array.filter() and Array.find() basic syntax
+// const newArray = Array.filter(callbackfn);
+// const value = Array.find(callbackfn);
+
+const things = [
+    {
+        id: 0,
+        title: 'whiskers on kittens',
+        favorite: true,
+        points: 97
+    },
+    {
+        id: 1,
+        title: 'raindrops on roses',
+        favorite: true,
+        points: 77
+    },
+    {
+        id: 2,
+        title: 'brown paper packages tied up with string',
+        favorite: true,
+        points: 87
+    },
+    {
+        id: 3,
+        title: 'dog bite',
+        favorite: false,
+        featured: 'true',
+        points: 12
+    },
+    {
+        id: 4,
+        title: 'bee sting',
+        favorite: false,
+        points: 6
+    }
+];
+
+// const myFavoriteThings = things.filter((thing) => thing.favorite);
+// console.log(myFavoriteThings);
+
+// const thingsWithOverFiftyPoints = things.filter((thing) => thing.points > 50);
+// console.log(thingsWithOverFiftyPoints);
+
+const selectThingByID = (id) => {
+return things.find((thing) => thing.id === id)
+};
+
+const selectThing = selectThingByID(4);
+console.log(selectThing);
